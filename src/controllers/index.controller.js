9c8600de665103523eef64e3f98b257b52b1246c
CarlosRoca13 <al361876@uji.es>
2020-03-04 15:04:56 +0100

    tunnel

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,12 +1,4 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: '',
-    database: 'prueba',
-    port: '5432'
-})
+const pool = require('./prueba')
 
 const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM users');
