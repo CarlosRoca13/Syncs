19828a2eec9e87855034c40223c5908552e88b07
CarlosRoca13 <al361876@uji.es>
2020-03-05 16:30:03 +0100

    Diseny físic

--- database/database.sql
+++ database/database.sql
@@ -1,11 +1,68 @@
-CREATE DATABASE prueba;
+CREATE TABLE User (
+    userId varchar(9)  NOT NULL,
+    name varchar(30)  NOT NULL,
+    lastName varchar(60)  NOT NULL,
+    email varchar(30)  NOT NULL,
+    username varchar(20)  NOT NULL,
+    password varchar(20)  NOT NULL,
+    verified char(1)  NOT NULL,
+    avatar varchar(50) NOT NULL,
+    birthday date NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (userId)
+    );
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(40),
-    email TEXT
-);
+CREATE TABLE Sheet (
+    sheetId varchar(9)  NOT NULL,
+    name varchar(30)  NOT NULL,
+    userId varchar(60)  NOT NULL,
+    description varchar(60)  NOT NULL,
+    pdf varchar(20)  NOT NULL,
+    key varchar(20)  NOT NULL,
+    mainGenre varchar(15)  NOT NULL,
+    likes int  NOT NULL,
+    dislikes int  NOT NULL,
+    views int  NOT NULL,
+    downloads int  NOT NULL,
+    CONSTRAINT sheet_pk PRIMARY KEY (sheetId),
+    CONSTRAINT sheet_Fk FOREIGN KEY (userId) references User ON DELETE RESTRICT ON UPDATE CASCADE
+    );
 
-INSERT INTO users (name, email) VALUES
-    ('joe', 'joe@ibm.com'),
-    ('ryan', 'ryan@gmail.com');
+CREATE TABLE SheetInstrument (
+    sheetId varchar(9)  NOT NULL,
+    instrument varchar(30)  NOT NULL,
+    effects varchar(60)  NOT NULL,
+    CONSTRAINT sheetInstrument_pk PRIMARY KEY (sheetId, instrument),
+    CONSTRAINT sheetInstrument_fk FOREIGN KEY (sheetId) references Sheet ON DELETE RESTRICT ON UPDATE CASCADE
+    );
+
+CREATE TABLE Playlist (
+    playlistId varchar(9)  NOT NULL,
+    userId varchar(9)  NOT NULL,
+    name varchar(60)  NOT NULL,
+    image varchar(50)  NOT NULL,
+    description varchar(60)  NOT NULL,
+    CONSTRAINT playlist_pk PRIMARY KEY (playlistId),
+    CONSTRAINT playlist_fk FOREIGN KEY (userId) references User ON DELETE RESTRICT ON UPDATE CASCADE
+    );
+
+CREATE TABLE PlaylistItem (
+    playlistId varchar(9)  NOT NULL,
+    sheetId varchar(9)  NOT NULL,
+    CONSTRAINT playlistItem_pk PRIMARY KEY (playlistId, sheetId),
+    CONSTRAINT playlistItem_fk FOREIGN KEY (playlistId) references Playlist ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT playlistItemSheet_fk FOREIGN KEY (sheetId) references Sheet ON DELETE RESTRICT ON UPDATE CASCADE
+    );
+
+CREATE TABLE Comment (
+    commentId varchar(9)  NOT NULL,
+    userId varchar(9)  NOT NULL,
+    sheetId varchar(9)  NOT NULL,
+    dateTime date NOT NULL,
+    description varchar(60)  NOT NULL,
+    response varchar(90)  NOT NULL,
+    likes int  NOT NULL,
+    dislikes int  NOT NULL,
+    CONSTRAINT playlistItem_pk PRIMARY KEY (commentId),
+    CONSTRAINT playlistItem_fk FOREIGN KEY (userId) references User ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT playlistItemSheet_fk FOREIGN KEY (sheetId) references Sheet ON DELETE RESTRICT ON UPDATE CASCADE
+    );
