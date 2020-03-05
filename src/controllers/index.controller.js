30f0baaa90ef8c51fc6ac5231b30bcf5d39d48e4
CarlosRoca13 <al361876@uji.es>
2020-03-05 16:24:09 +0100

    BBDD LOCAL

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,4 +1,12 @@
-const { pool } = require('./tunnel.js')
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: '',
+    database: 'prueba',
+    port: '5432'
+}) 
 
 const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM users');
