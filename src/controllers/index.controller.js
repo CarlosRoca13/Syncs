7df60083bac71c1fd319999a43c0b2cfaaa67ba1
CarlosRoca13 <al361876@uji.es>
2020-03-05 18:54:42 +0100

    Dao partituras

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -70,11 +70,84 @@ const deleteClient = async (req, res) => {
     res.send('Client ' + id + ' deleted succesfully');
 }
 
+// Sheets --
+const getSheets = async (req, res) => {
+    const response = await pool.query('SELECT * FROM sheet');
+    res.status(200).json(response.rows);  
+};
+
+const getSheetById = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM sheet WHERE sheetid = $1', [id]);
+
+    res.json(response.rows);
+}
+
+const createSheet = async (req, res) => {
+    const { name, clientId, description, pdf, key, mainGenre, likes, dislikes, views, downloads, image } = req.body;
+
+    const response = await pool.query('INSERT INTO sheet (name, clientId, description, pdf, key, mainGenre, likes, dislikes, views, downloads, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)', [
+        name, 
+        clientId, 
+        description, 
+        pdf, 
+        key, 
+        mainGenre, 
+        likes, 
+        dislikes, 
+        views, 
+        downloads, 
+        image
+    ]);
+    console.log(response);
+    res.json({
+        message: "Sheet Added Succesfully",
+        body: {
+            sheet: {name, clientId, description, pdf, key, mainGenre, likes, dislikes, views, downloads, image}
+        }
+    })
+};
+
+const updateSheet = async (req, res) => {
+    const id = req.params.id;
+    const { name, clientId, description, pdf, key, mainGenre, likes, dislikes, views, downloads, image } = req.body;
+
+    response = await pool.query('UPDATE Sheet SET name = $1, clientId = $2, description = $3, pdf = $4, key = $5, mainGenre = $6, likes = $7, dislikes = $8, views = $9, downloads = $10, image = $11 WHERE sheetid = $12', [
+        name, 
+        clientId, 
+        description, 
+        pdf, 
+        key, 
+        mainGenre, 
+        likes, 
+        dislikes, 
+        views, 
+        downloads, 
+        image,
+        id
+    ]);
+    console.log(response);
+    res.json('Sheet Updated Succesfully');
+}
+
+const deleteSheet = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('DELETE FROM Sheet WHERE sheetid = $1', [id]);
+    console.log(response);
+    
+    res.send('Sheet ' + id + ' deleted succesfully');
+}
+
 module.exports = {
     getClients,
     getClientById,
     createClient,
     updateClient,
-    deleteClient
+    deleteClient,
+    getSheets,
+    getSheetById,
+    createSheet,
+    updateSheet,
+    deleteSheet
 }    
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getClients, getClientById, createClient, updateClient, deleteClient } = require('../controllers/index.controller');
+const { getClients, getClientById, createClient, updateClient, deleteClient, getSheets, getSheetById, createSheet, updateSheet, deleteSheet } = require('../controllers/index.controller');
 
 // Clients --
 router.get('/clients', getClients);
@@ -9,5 +9,12 @@ router.get('/clients/:id', getClientById);
 router.post('/clients', createClient);
 router.put('/clients/:id', updateClient);
 router.delete('/clients/:id', deleteClient);
+
+// Sheets --
+router.get('/sheets', getSheets);
+router.get('/sheets/:id', getSheetById);
+router.post('/sheets', createSheet);
+router.put('/sheets/:id', updateSheet);
+router.delete('/sheets/:id', deleteSheet);
 
 module.exports = router;
