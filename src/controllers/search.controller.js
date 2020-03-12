d7a5978ccbb35c7a1dec37f9e1ca4af2667a9360
Rgnar98 <al361915@uji.es>
2020-03-12 19:10:10 +0100

    Modificación controlador search

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -2,8 +2,8 @@ const { pool } = require('./index.controller');
 
 const getSearch = async (req, res) =>{
     const name = req.params.body;
-    name = name.toLowerCase();
-    const response = await pool.query('SELECT s.name, c.name, p.name FROM sheet as s FULL JOIN client as c USING(name) FULL JOIN playlist as p USING(name)WHERE name like %$1%', [name]);
+    //name = name.toLowerCase();
+    const response = pool.query('SELECT s.name, c.name, p.name FROM sheet as s FULL JOIN client as c USING(name) FULL JOIN playlist as p USING(name)WHERE name like $1', ['%' + name + '%']);
 
     res.json(response.rows);
 
