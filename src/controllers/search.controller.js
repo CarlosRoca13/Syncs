c0b6f5bdd2ba701126231d3f8cd4b859e45dffeb
Rgnar98 <al361915@uji.es>
2020-03-16 10:31:33 +0100

    search controller functional

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,11 +1,12 @@
 const { pool } = require('./index.controller');
 
 const getSearch = async (req, res) =>{
-    const name = req.params.body;
+    const { name }= req.body;
     //name = name.toLowerCase();
-    const response = pool.query('SELECT s.name, c.name, p.name FROM sheet as s FULL JOIN client as c USING(name) FULL JOIN playlist as p USING(name)WHERE name like $1', ['%' + name + '%']);
+    const response = await pool.query("SELECT COALESCE(s.name, 'NO RESULTS') as sheet, COALESCE(c.name, 'NO RESULTS') as client, COALESCE(p.name, 'NO RESULTS') as playlist FROM sheet as s FULL JOIN client as c USING(name) FULL JOIN playlist as p USING(name)WHERE name like $1", ['%' + name + '%']);
 
-    res.json(response.rows);
+    res.status(200).json(response.rows);
+        
 
 } //probar funcionamiento
 
