3405c8aa33db72d7507c5136ee2cf167492ae1fe
CarlosRoca13 <al361876@uji.es>
2020-05-11 15:56:00 +0200

    cambis mario

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -16,26 +16,16 @@ class SearchController extends ApiController
      */
     public function searchartist($name)
     {
-        return DB::select('SELECT COALESCE(c.username, NULL) as client, COALESCE(c.id, NULL) as clientId FROM clients as c WHERE UPPER(username) like UPPER(:name)',['name'=> '%' . $name . '%']);    
+        return DB::select('SELECT COALESCE(c.username, NULL) as client, COALESCE(c.id, NULL) as clientId, COALESCE(c.avatar, NULL) as avatar FROM clients as c WHERE UPPER(username) like UPPER(:name)',['name'=> '%' . $name . '%']);    
     }
         
     public function searchsong($name)
     {
-        return DB::select('SELECT COALESCE(s.name, NULL) as sheet, COALESCE(s.id, NULL) as sheetid FROM sheets as s WHERE UPPER(name) like UPPER(:name)',['name'=> '%' . $name . '%']);    
-    }
-        
-    public function searchplaylist($name)
-    {
-        return DB::select('SELECT COALESCE(p.name, NULL) as playlist, COALESCE(p.id, NULL) as playlistId FROM playlists as p WHERE UPPER(name) like UPPER(:name)',['name'=> '%' . $name . '%']);    
+        return DB::select('SELECT COALESCE(s.name, NULL) as sheet, COALESCE(s.id, NULL) as sheetid, (SELECT username FROM clients WHERE id = s.clients_id) as client FROM sheets as s WHERE UPPER(name) like UPPER(:name)',['name'=> '%' . $name . '%']);    
     }
 
     public function getsongartist($id)
     {
         return DB::select('SELECT c.username as client FROM clients as c  JOIN sheets as s ON(s.clients_id = c.id) WHERE s.id = :sheetid',['sheetid' => $id]);
-    }
-
-    public function getplaylistartist($id)
-    {
-        return DB::select('SELECT c.username as client FROM clients as c JOIN playlists as p ON(p.clients_id = c.id) WHERE p.id = :playlistid',['playlistid' => $id]);
     }
 }
